67840c6424197d0bec6135497b9bbc117a1106b7
Andretreccia <andreabtreccia@gmail.com>
2022-01-11 15:04:47 +0100

    Create a controller and a model

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>Movies</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, inventore.</p>
+</div>
+
+@endsection
