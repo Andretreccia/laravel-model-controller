66f994fe69347e2a8180fbd60b2785986cabc37b
Andretreccia <andreabtreccia@gmail.com>
2022-01-11 15:23:14 +0100

    Create cards dinamically taking data from the current database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,17 @@
 @section('content')
 
 <div class="container">
-    <h1>Movies</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, inventore.</p>
+   @foreach ($movies as $movie)
+       <div class="row">
+           <div class="card">
+               <h4>Title: {{$movie->title}}</h4>
+               <p>Original Title: {{$movie->original_title}}</p>
+               <p>Nationality: {{$movie->nationality}}</p>
+               <p>Relase Date: {{$movie->date}}</p>
+               <p>Rating: {{$movie->vote}}</p>
+           </div>
+       </div>
+   @endforeach
 </div>
 
 @endsection
