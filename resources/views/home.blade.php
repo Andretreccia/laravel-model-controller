58535d7de1f9874829fd8899f9bf950f7df48502
Andretreccia <andreabtreccia@gmail.com>
2022-01-11 16:39:10 +0100

    Add style with scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="banner">
+   <img src="https://www.clipartmax.com/png/full/310-3105859_film-cinema-icon-png.png" alt="">
+   <h1>Welcome Home</h1>
+   <img src="https://www.clipartmax.com/png/full/310-3105859_film-cinema-icon-png.png" alt="">
+</div>
 <div class="container">
-   @foreach ($movies as $movie)
-       <div class="row">
+    <div class="row">
+           @foreach ($movies as $movie)
            <div class="card">
-               <h4>Title: {{$movie->title}}</h4>
-               <p>Original Title: {{$movie->original_title}}</p>
-               <p>Nationality: {{$movie->nationality}}</p>
-               <p>Relase Date: {{$movie->date}}</p>
-               <p>Rating: {{$movie->vote}}</p>
+               <h3>{{$movie->title}}</h3>
+               <p>Titolo originale: <span>{{$movie->original_title}}</span></p>
+               <p>Nazionalità: <span>{{$movie->nationality}}</span></p>
+               <p>Data di uscita: <span>{{$movie->date}}</span></p>
+               <p>Voto: <span>{{$movie->vote}}</span></p>
+               
            </div>
+           @endforeach
        </div>
-   @endforeach
 </div>
 
 @endsection
